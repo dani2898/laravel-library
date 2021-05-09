d222d4d7d4c9362181c1f21f42b3e843698bd131
Daniela <danielavilla2898@gmail.com>
2021-05-08 23:08:59 -0500

    Model relations, auth
    Creating relations between models, creating auth folders

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+    public $fillable = ['name', 'description'];
+
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }   
 }
